3a1e3ecd90f2710c959c611bfd5ad88736825fe9
Cyrine-afk <cyrine.trabelsi@esprit.tn>
2023-02-22 10:33:25 +0100

    Application.java update

--- src/main/java/tn/esprit/CampusConnectApplication.java
+++ src/main/java/tn/esprit/CampusConnectApplication.java
@@ -7,5 +7,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableScheduling
 @SpringBootApplication
 public class CampusConnectApplication {
-
+    public static void main(String[] args) {
+        SpringApplication.run(CampusConnectApplication.class, args);
+    }
 }
